04293186f05cb004fa0e5966de18929e9bf72f33
Florian Siffer <f.siffer@gmail.com>
2019-11-25 16:56:10 +0100

    major progrss on plan position tool

--- envelop/src/plan_position/plan_position_tool.rb
+++ envelop/src/plan_position/plan_position_tool.rb
@@ -1,49 +1,62 @@
+# frozen_string_literal: true
+
 module Envelop
   module PlanPositionTool
     class PlanPositionTool
+      PHASES = { BEFORE_FIRST_POINT: 0, BEFORE_MOVE: 1, BEFORE_SECOND_POINT: 2, BEFORE_SCALE: 3, FINISHED: 10 }.freeze
 
       def initialize(image_obj)
         @image_obj = image_obj
       end
 
       def activate
-    	  reset_tool
-
-        puts 'activated PlanPositionTool'
+        puts 'activating PlanPositionTool...'
+        reset_tool
       end
 
       def deactivate(view)
-        Sketchup.active_model.active_entities.erase_entities @image_obj if @image_obj
+        puts 'deactivating PlanPositionTool...'
+
+        # no need to reset_tool, tool instance will be discarded after this
+
+        if @phase != PHASES[:FINISHED]
+          if @image_obj
+            Sketchup.active_model.active_entities.erase_entities @image_obj
+          end
+        end
         view.invalidate
       end
 
-    	def resume(view)
-        puts 'Resuming PlanPositionTool'
+      def resume(view)
+        puts 'resuming PlanPositionTool...'
 
-    		update_ui
-    		view.invalidate
-    	end
-
-      def suspend(view)
-        puts 'Suspending PlanPositionTool'
+        set_status_text
+        view.invalidate
       end
 
-      def onCancel(reason, view)
-      		# reset_tool TODO: is removing the image and deactivating the tool the correct behaviour? should it instead just reset before the frist click?
-          Sketchup.active_model.select_tool(nil)
-      		view.invalidate
+      def suspend(_view)
+        puts 'suspending PlanPositionTool...'
       end
 
-    	def draw(view)
-    		draw_preview(view)
-    		@mouse_ip.draw(view) if @mouse_ip.display?
-    	end
+      def onCancel(_reason, _view)
+        # reset_tool TODO: is removing the image and deactivating the tool the correct behaviour? should it instead just reset before the frist click?
+
+        Sketchup.active_model.select_tool(nil) # this will invalidate view & delete becasue phase will not be FINISHED
+      end
+
+      def draw(view)
+        if @phase == PHASES[:BEFORE_MOVE] || @phase == PHASES[:BEFORE_SCALE]
+          draw_preview_line(view)
+        end
+
+        @mouse_ip.draw(view) if @mouse_ip.display?
+      end
 
       def getExtents
-        if @first_point && @mouse_ip.valid?
+        if (@phase == PHASES[:BEFORE_MOVE] || @phase == PHASES[:BEFORE_SCALE]) && @mouse_ip.valid?
           bb = Geom::BoundingBox.new
           bb.add(@first_point.position, @mouse_ip.position)
-          return bb
+          bb
         end
       end
 
@@ -59,96 +72,128 @@ module Envelop
         view.invalidate
       end
 
+      VK_RETURN = 13
+      def onKeyDown(key, repeat, flags, view)
+        if key == VK_RETURN
+          finish()
+        end
+      end
+
       def onLButtonDown(flags, x, y, view)
-        if not @first_point
+        if @phase == PHASES[:BEFORE_FIRST_POINT]
           if @mouse_ip.valid?
-            @first_point = Sketchup::InputPoint.new(@mouse_ip.position)
-            puts "first_point: #{@first_point.position}"
+            @first_point.copy!(@mouse_ip)
+            puts "first_point to move: #{@first_point.position}"
+            @phase = PHASES[:BEFORE_MOVE]
           else
-            UI.messagebox('Could not get first point because current mouse position is invalid InputPosition')
+            UI.messagebox('Could not get first point to move because current mouse position is invalid InputPosition.')
           end
-        else
+        elsif @phase == PHASES[:BEFORE_MOVE]
           if @mouse_ip.valid?
-            puts "second_point: #{@mouse_ip.position}"
+            puts "second_point to move: #{@mouse_ip.position}"
             move_image(@mouse_ip)
+            @move_target.copy!(@mouse_ip)
+            @phase = PHASES[:BEFORE_SECOND_POINT]
           else
-            UI.messagebox('Could not get second point because current mouse position is invalid InputPosition')
+            UI.messagebox('Could not get second point to move to because current mouse position is invalid InputPosition.')
+          end
+        elsif @phase == PHASES[:BEFORE_SECOND_POINT]
+          if @mouse_ip.valid?
+            @first_point.copy!(@mouse_ip)
+            puts "first_point to scale: #{@first_point.position}"
+            @phase = PHASES[:BEFORE_SCALE]
+          else
+            UI.messagebox('Could not get first point to scale because current mouse position is invalid InputPosition.')
+          end
+        elsif @phase == PHASES[:BEFORE_SCALE]
+          if @mouse_ip.valid?
+            puts "second_point to scale: #{@mouse_ip.position}"
+            scale_image(@mouse_ip)
+            finish()
+          else
+            UI.messagebox('Could not get second point to scale to because current mouse position is invalid InputPosition.')
           end
         end
       end
-      #
-    	# 	if picked_first_point?
-      #
-    	# 		object_second = @mouse_ip.instance_path.root
-    	# 		unless object_second.nil? || object_second == @object_first
-      #
-    	# 			puts "picked_second_ip: #{@mouse_ip.position}"
-      #
-    	# 			# move first object to second object
-    	#
-    	# 			model = Sketchup.active_model
-    	# 			model.start_operation('IP6 Move Tool', true)
-      #
-    	# 			@object_first.transformation = translation * @object_first.transformation
-      #
-    	# 			model.commit_operation
-      #
-    	# 			reset_tool
-    	# 		end
-    	# 	else
 
       private
 
-      def move_image(second_point)
-				vec = second_point.position - @first_point.position
-				trans = Geom::Transformation.translation(vec)
-        @image_obj.transform!(trans)
+      def scale_image(second_point)
+        # assumes @first_point.valid? && @first_point set to useful position
+        # assumes @move_target.valid? && @move_target set to useful position
 
-        finish
+        current_length = @move_target.position.distance(@first_point.position) # TODO: NOW: THIS DOES NOT WORKL?
+        target_length = @move_target.position.distance(second_point.position)
+        ratio = target_length / current_length
+
+        puts "current_length: #{current_length}"
+        puts "target_length: #{target_length}"
+        puts "ratio: #{ratio}"
+
+        trans = Geom::Transformation.scaling(@move_target.position, ratio)
+        @image_obj.transform!(trans)
+      end
+
+      def move_image(second_point)
+        # assumes @first_point.valid? && @first_point set to useful position
+
+        vec = second_point.position - @first_point.position
+        trans = Geom::Transformation.translation(vec)
+        @image_obj.transform!(trans)
       end
 
       def finish
-        remove_instance_variable(:@image_obj)
+        @phase = PHASES[:FINISHED]
 
         model = Sketchup.active_model
         model.selection.clear
         model.select_tool(nil)
       end
 
-      def draw_preview(view)
-        if @first_point && @mouse_ip.valid?
-      		view.set_color_from_line(@first_point.position, @mouse_ip.position)
-      		view.line_width = 1
-      		view.line_stipple = ''
-      		view.draw(GL_LINES, [@first_point.position, @mouse_ip.position])
+      def draw_preview_line(view)
+        # assumes @first_point.valid? && @first_point set to useful position
+
+        if @mouse_ip.valid?
+          view.set_color_from_line(@first_point.position, @mouse_ip.position)
+          view.line_width = 1
+          view.line_stipple = ''
+          view.draw(GL_LINES, [@first_point.position, @mouse_ip.position])
         end
       end
 
       def reset_tool
+        # reset selection
         selection = Sketchup.active_model.selection
-      	selection.clear
+        selection.clear
         selection.add(@image_obj)
 
+        # reset state
+        @phase = PHASES[:BEFORE_FIRST_POINT]
         @mouse_ip = Sketchup::InputPoint.new
-        remove_instance_variable(:@first_point) if @first_point
+        @first_point = Sketchup::InputPoint.new
+        @move_target = Sketchup::InputPoint.new
 
-        update_ui
+        set_status_text
       end
 
-      def update_ui
-        if !@first_point
-          Sketchup.status_text = 'Click point on new plan. "Esc" to abort.'
-        else
-          Sketchup.status_text = 'Click point on old plan to move new plan. "Esc" to abort.'
+      def set_status_text
+        if @phase == PHASES[:BEFORE_FIRST_POINT]
+          Sketchup.status_text = 'Click point on new plan. "Esc" to abort. "Enter" to accept new plan as is.'
+        elsif @phase == PHASES[:BEFORE_MOVE]
+          Sketchup.status_text = 'Click point on old plan to move new plan. "Esc" to abort. "Enter" to accept new plan as is.'
+        elsif @phase == PHASES[:BEFORE_SECOND_POINT]
+          Sketchup.status_text = 'Click second point on new plan. "Enter" to accept new plan as is. "Esc" to abort.'
+        elsif @phase == PHASES[:BEFORE_SCALE]
+          Sketchup.status_text = 'Click second point on old plan to scale new plan. "Enter" to accept new plan as is. "Esc" to abort.'
         end
       end
     end
 
     def self.activate_plan_position_tool(image_obj)
-    	Sketchup.active_model.select_tool(Envelop::PlanPositionTool::PlanPositionTool.new(image_obj))
+      Sketchup.active_model.select_tool(Envelop::PlanPositionTool::PlanPositionTool.new(image_obj))
     end
 
-    def self.reload()
+    def self.reload
       Sketchup.active_model.select_tool(nil)
     end
     reload
