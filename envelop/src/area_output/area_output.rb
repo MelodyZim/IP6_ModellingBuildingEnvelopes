0c7d5661cef43ea6957e7b19ad6d19ed6ba2906a
Patrick Ackermann <patrick.ackermann@students.fhnw.ch>
2019-11-26 10:22:17 +0100

    Add Boilerplate for Area Output dialog

---
+++ envelop/src/area_output/area_output.rb
@@ -0,0 +1,74 @@
+require_relative '../vendor/rb/image_size'
+
+module Envelop
+  module AreaOutput
+		# public
+
+		def self.open_dialog()
+			@house = Envelop::ModelingTool.search_house
+			show_dialog
+		end
+
+		private
+
+		def self.create_dialog
+			html_file = File.join(__dir__, 'area_output.html')
+			options = {
+				:dialog_title => "Area Output",
+				:preferences_key => "envelop.areaoutput",
+				:style => UI::HtmlDialog::STYLE_DIALOG,
+				:resizable => false,
+				:width => 500,
+				:height => 500,
+			}
+			dialog = UI::HtmlDialog.new(options)
+			dialog.set_size(options[:width], options[:height]) # Ensure size is set.
+			dialog.set_file(html_file)
+			dialog.center
+			dialog
+		end
+
+		def self.show_dialog
+      if @dialog&.visible?
+        @dialog.bring_to_front
+      else
+        @dialog ||= create_dialog
+        # @dialog.add_action_callback("say") { |action_context, text|
+          # puts "html > #{text}"
+          # nil
+        # }
+        # @dialog.add_action_callback("ready") { |action_context|
+          # self.set_image
+          # nil
+        # }
+        # @dialog.add_action_callback("accept") { |action_context, image_base64, orientation|
+          # puts "plan_edit accept: orientation=#{orientation}"
+          # Envelop::PlanPosition.add_image(image_base64, orientation)
+          # @dialog.close
+          # nil
+        # }
+        # @dialog.add_action_callback("cancel") { |action_context|
+          # @dialog.close
+          # nil
+        # }
+        @dialog.show
+      end
+
+			@dialog.show
+		end
+
+		# def self.set_image
+			# return if @dialog.nil?
+			# puts "ruby > set_image callback"
+			# @dialog.execute_script("setImage('#{@image_base64}')")
+		# end
+
+    def self.reload
+      if @dialog
+        @dialog.close
+        remove_instance_variable(:@house)
+      end
+    end
+    reload
+  end
+end
