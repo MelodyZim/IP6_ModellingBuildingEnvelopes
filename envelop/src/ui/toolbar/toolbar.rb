e23affcef7dc0c70bd11a73a0f95f09010f50a15
Florian Siffer <f.siffer@gmail.com>
2019-12-09 13:57:57 +0100

    toolbar fixes

--- envelop/src/ui/toolbar/toolbar.rb
+++ envelop/src/ui/toolbar/toolbar.rb
@@ -2,13 +2,13 @@ require 'sketchup'
 
 module Envelop
   module Toolbar
-  
+
     def self.area_command
       cmd = UI::Command.new("Area") {
-        Envelop::AreaOutput.open_dialog(Envelop::ModelingTool.search_house)
+        Envelop::AreaOutput.open_dialog(Envelop::Housekeeper.get_house)
       }
       cmd.set_validation_proc {
-        if Envelop::ModelingTool.search_house.nil?
+        if Envelop::Housekeeper.get_house.nil?
           MF_GRAYED
         else
           MF_ENABLED
@@ -19,10 +19,10 @@ module Envelop
       cmd.tooltip = "Area Output"
       cmd.status_bar_text = "Calculate surface area of selection"
       cmd.menu_text = "Area"
-      
+
       return cmd
     end
-    
+
     def self.reload_command
       cmd = UI::Command.new("Reload") {
         Envelop.reload
@@ -32,10 +32,10 @@ module Envelop
       cmd.tooltip = "Envelop.reload"
       cmd.status_bar_text = "Reload the envelop plugin"
       cmd.menu_text = "Reload"
-      
+
       return cmd
     end
-    
+
     def self.modeling_tool_add_command
       cmd = UI::Command.new("Add") {
         Envelop::ModelingTool.add_selection
@@ -52,10 +52,10 @@ module Envelop
       cmd.tooltip = "Add Selection"
       cmd.status_bar_text = "Add selection to house"
       cmd.menu_text = "Add"
-      
+
       return cmd
     end
-    
+
     def self.modeling_tool_subtract_command
       cmd = UI::Command.new("Subtract") {
         Envelop::ModelingTool.subtract_selection
@@ -72,7 +72,7 @@ module Envelop
       cmd.tooltip = "Subtract Selection"
       cmd.status_bar_text = "Subtract selection from house"
       cmd.menu_text = "Subtract"
-      
+
       return cmd
     end
 
@@ -91,7 +91,7 @@ module Envelop
 
     unless file_loaded?(__FILE__)
       @toolbar = UI::Toolbar.new "Envelop Toolbar"
-    
+
       @toolbar = @toolbar.add_item area_command
       @toolbar = @toolbar.add_separator
       @toolbar = @toolbar.add_item modeling_tool_add_command
@@ -99,10 +99,10 @@ module Envelop
       @toolbar = @toolbar.add_item scale_tool_command
       @toolbar = @toolbar.add_separator
       @toolbar = @toolbar.add_item reload_command
-      
+
       file_loaded(__FILE__)
     end
-    
+
     @toolbar.show
   end
 end
