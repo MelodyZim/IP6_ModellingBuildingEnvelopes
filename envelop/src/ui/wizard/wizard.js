736f3df022fa275afa80bb9b51afea4cfe06f41b
Florian Siffer <f.siffer@gmail.com>
2020-01-14 11:58:21 +0100

    adds substeps to wizard

--- envelop/src/ui/wizard/wizard.js
+++ envelop/src/ui/wizard/wizard.js
@@ -1,5 +1,6 @@
 $(function() {
   $("#step_template").hide();
+  $("#substep_template").hide();
 
   sketchup.call_set_content();
 });
@@ -10,17 +11,47 @@ function set_content(content) {
 }
 
 function add_step(step) {
-    step_div = new_step_div();
+  step_div = new_step_div();
 
-    step_div.find('.number').html(step.number);
-    step_div.find('.title').html(step.title);
+  step_div.find('.number').html(step.number);
+  step_div.find('.title').html(step.title);
+
+  if (step.hasOwnProperty("sublist")) {
+    step_div.addClass("setp_with_substeps");
+
+    last_substep = step.sublist.pop();
+    step.sublist.forEach(substep => add_substep(step.number, substep));
+    add_substep(step.number, last_substep).addClass("last_substep");
+  }
 }
 
-var step_div_counter = 0;
-function new_step_div() {
-  step_div_counter++;
+function add_substep(number, step) {
+  substep_div = new_substep_div();
 
-  var $clone = $("#step_template").clone().prop('id', 'step-div-' + step_div_counter);
+  substep_div.find('.number').html(number + "." + step.number);
+  substep_div.find('.title').html(step.title);
+
+  return substep_div;
+}
+
+var div_counter = 0;
+
+function new_step_div() {
+  div_counter++;
+
+  var $clone = $("#step_template").clone().prop('id', 'step-div-' + div_counter);
+
+  $("#steps_container").append($clone);
+
+  $clone.show();
+
+  return $clone;
+}
+
+function new_substep_div() {
+  div_counter++;
+
+  var $clone = $("#substep_template").clone().prop('id', 'substep-div-' + div_counter);
 
   $("#steps_container").append($clone);
 
