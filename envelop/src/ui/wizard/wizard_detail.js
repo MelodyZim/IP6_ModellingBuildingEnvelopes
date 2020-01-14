1a0b1c3b3da085927e0a8a8e23d306118c4f50d5
Florian Siffer <f.siffer@gmail.com>
2020-01-14 12:52:01 +0100

    profgress on detial wizard

--- envelop/src/ui/wizard/wizard_detail.js
+++ envelop/src/ui/wizard/wizard_detail.js
@@ -1,7 +1,14 @@
 $(function() {
+  $(this).keydown(function(e) {
+    if (e.keyCode == 27) {
+      sketchup.close();
+    }
+  });
+
   sketchup.call_set_content();
 });
 
 function set_content(content) {
-  console.log(content);
+  content = JSON.parse(content);
+  $('#movie_src').attr('src', content.video);
 };
