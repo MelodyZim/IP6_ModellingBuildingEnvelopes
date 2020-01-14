58f76ab8c18604f8ed4fcbd6d317510db69944ec
Florian Siffer <f.siffer@gmail.com>
2020-01-14 15:50:16 +0100

    wizard detail done

--- envelop/src/ui/wizard/wizard_detail.js
+++ envelop/src/ui/wizard/wizard_detail.js
@@ -10,5 +10,18 @@ $(function() {
 
 function set_content(content) {
   content = JSON.parse(content);
+  console.log(content);
+
+  console.log(`${content.forNumber} ${content.title}`);
+  $('#title').html(`${content.forNumber} ${content.title}`);
+
   $('#movie_src').attr('src', content.video);
+  $('#movie')[0].load();
+  $('#movie')[0].play();
+
+  content.steps.forEach(add_step);
 };
+
+function add_step(step) {
+  $('#steps').append(`<li>${step}</li>`);
+}
